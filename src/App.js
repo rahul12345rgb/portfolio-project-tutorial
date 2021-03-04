c7bcce5365e4e34b32c6894f4c32e2ec9fea9c7e
rahul12345rgb <58728008+rahul12345rgb@users.noreply.github.com>
2021-03-04 09:06:01 +0530

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Component/Navbar'
 function App() {
   return (
-  <h2>Portfolio Project...</h2>
+  <Navbar />
   ) 
 }
 
